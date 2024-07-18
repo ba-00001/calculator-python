b244b1fce7663dfb8b2bdc5bb2f1a63bfe2951f9
BRIAN BAZURTO <124537281+ba-00001@users.noreply.github.com>
2024-07-18 11:18:48 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,31 @@
 def gather_inputs():
-    operation = input("Please type in operation you would like to complete: +, -, *, or /")
-    number_1 = int(input('Please enter the first number: '))
-    number_2 = int(input('Please enter the second number: '))
+    """
+    Gathers input from the user for the operation and numbers.
+    Returns:
+        tuple: operation (str), number_1 (int), number_2 (int)
+    """
+    # Gather operation and numbers from the user
+    operation = input("Please type in operation you would like to complete: +, -, *, or /: ")
+    try:
+        number_1 = int(input('Please enter the first number: '))
+        number_2 = int(input('Please enter the second number: '))
+    except ValueError:
+        raise ValueError("Please enter valid numbers.")
     return operation, number_1, number_2
 
-def run_operation(number_1,  number_2, operation):
+def run_operation(number_1, number_2, operation):
+    """
+    Executes the given operation on the provided numbers.
+    Args:
+        number_1 (int): The first number.
+        number_2 (int): The second number.
+        operation (str): The operation to be performed (+, -, *, /).
+    Returns:
+        float: The result of the operation.
+    Raises:
+        Exception: If an invalid operator is provided.
+        ZeroDivisionError: If division by zero is attempted.
+    """
     if operation == '+':
         print('{} + {} = '.format(number_1, number_2), end = '')
         return number_1 + number_2
@@ -15,15 +36,20 @@ def run_operation(number_1,  number_2, operation):
         print('{} * {} = '.format(number_1, number_2), end = '')
         return number_1 * number_2
     elif operation == '/':
+        if number_2 == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         print('{} / {} = '.format(number_1, number_2), end = '')
         return number_1 / number_2
     else:
         raise Exception('This operator is invalid. Try running the program again.')
 
 def main():
+    """
+    Main function to run the calculator program.
+    """
     print("Welcome")
     operation, number_1, number_2 = gather_inputs()
     print(run_operation(number_1, number_2, operation))
 
 if __name__ == "__main__":
-	main()
+    main()
